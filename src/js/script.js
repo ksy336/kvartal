a6a924f18c89bff51e1e9392e3da983c180f1fe6
ksy336 <ksuoxy773@gmail.com>
2022-07-19 12:52:22 +0400

    feat: add modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,50 @@
 "use strict";
 
+// modal
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('button');
+
+const openModal = function (e) {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function (e) {
+    e.preventDefault();
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => {
+    btn.addEventListener("click", openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
 // add new info
-const addNewInfo = (e) => {
-    const descriptions = document.querySelectorAll(".city-description");
-    const invisibleText = document.querySelectorAll(".info-invisible");
+const addNewInfo = () => {
+    const container = document.querySelector(".info-layout");
+    container.addEventListener("click", function (e) {
+        const parent = e.target.closest('.info-layout');
+        const text = parent.querySelectorAll('.info-invisible');
+        console.log(text);
 
+        if (text) {
+            text.forEach((textItem) => {
+                textItem.classList.toggle("visible");
+            })
+        }
 
-    descriptions.forEach((card) => {
-        card.addEventListener("click", function (e) {
-                invisibleText.forEach((text) => {
-                        text.classList.toggle("visible");
-                })
-
-        });
     })
 }
 addNewInfo();
